a4232676dfe62780659e67ee83d865358a3e982a
rsouthward <rysouth32@gmail.comm>
2021-01-01 12:32:09 -0600

    Working image dots, and working collapsibles

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const imagesSchema = new mongoose.Schema({
   variant_ids: Array,
   src: String,
   width: Number,
-  height: Number,
+  height: Number
 })
 const featuredImagesSchema = new mongoose.Schema({
   id: Number,
@@ -62,7 +62,7 @@ const variantSchema = new mongoose.Schema({
   position: Number,
   product_id: Number,
   created_at: String,
-  updated_at: String,
+  updated_at: String
 })
 const productSchema = new mongoose.Schema({
   id: Number,
@@ -85,7 +85,7 @@ const brandSchema = new mongoose.Schema({
     required: [true, "Error: Brand Name not included."]
   },
   brandImageURL:{
-    type: String,
+    type: Array,
     required: [true, "Error: brandImageURL not included."]
   },
   brandDesc:{
@@ -103,9 +103,10 @@ const brandSchema = new mongoose.Schema({
   brandLinkInternal:{
     type: String
   },
-  products: [productSchema],
   category: String,
-  cause: String
+  cause: String,
+  brandProductTypes: Array,
+  products: [productSchema]
 })
 
 
@@ -130,225 +131,84 @@ app.get("/", function(req, res){
 })
 });
 
-// var test = new Brand({
-// brandName:  "test1",
-// brandImageURL:  "testimage.png",
-// brandDesc:  "This is test data",
-// brandTags:  "test",
-// brandLinkRedirect:  "test.com",
-// brandLinkInternal:  "/test",
-// products: [{
-// id: 4452928487466,
-// title: "Choose Bar 3",
-// handle: "choose-bar-34",
-// body_html: "<strong>***This is a hidden product used with the Product Options application, Please do not delete this product.***</strong>",
-// published_at: "2020-05-04T01:02:03-04:00",
-// created_at: "2020-05-04T01:02:03-04:00",
-// updated_at: "2020-11-29T01:51:04-05:00",
-// vendor: "Herb'N Eden",
-// product_type: "OPTIONS_HIDDEN_PRODUCT",
-// tags: [
-// "BOLD_HIDDEN_PRODUCT",
-// "OPTIONS_HIDDEN_PRODUCT",
-// ],
-// variants: [
-// {
-// id: 31543212933162,
-// title: "Rosewater Kefir & Lavender",
-// option1: "Rosewater Kefir & Lavender",
-// option2: null,
-// option3: null,
-// sku: "",
-// requires_shipping: true,
-// taxable: true,
-// featured_image: null,
-// available: true,
-// price: "1.00",
-// grams: 0,
-// compare_at_price: null,
-// position: 1,
-// product_id: 4452928487466,
-// created_at: "2020-05-04T01:02:03-04:00",
-// updated_at: "2020-11-28T17:23:31-05:00",
-// },
-// {
-// id: 31543212965930,
-// title: "Grapefruit & Himalayan Pink",
-// option1: "Grapefruit & Himalayan Pink",
-// option2: null,
-// option3: null,
-// sku: "",
-// requires_shipping: true,
-// taxable: true,
-// featured_image: null,
-// available: true,
-// price: "1.00",
-// grams: 0,
-// compare_at_price: null,
-// position: 2,
-// product_id: 4452928487466,
-// created_at: "2020-05-04T01:02:04-04:00",
-// updated_at: "2020-11-29T01:46:41-05:00",
-// },
-// {
-// id: 31543212998698,
-// title: "Vanilla Mint Coffee",
-// option1: "Vanilla Mint Coffee",
-// option2: null,
-// option3: null,
-// sku: "",
-// requires_shipping: true,
-// taxable: true,
-// featured_image: null,
-// available: true,
-// price: "1.00",
-// grams: 0,
-// compare_at_price: null,
-// position: 3,
-// product_id: 4452928487466,
-// created_at: "2020-05-04T01:02:05-04:00",
-// updated_at: "2020-11-29T00:00:40-05:00",
-// },
-// {
-// id: 31543213064234,
-// title: "Cedarwood & Sea Clay",
-// option1: "Cedarwood & Sea Clay",
-// option2: null,
-// option3: null,
-// sku: "",
-// requires_shipping: true,
-// taxable: true,
-// featured_image: null,
-// available: true,
-// price: "1.00",
-// grams: 0,
-// compare_at_price: null,
-// position: 4,
-// product_id: 4452928487466,
-// created_at: "2020-05-04T01:02:06-04:00",
-// updated_at: "2020-11-28T21:07:26-05:00",
-// },
-// ],
-// images: [ ],
-// options: [
-// {
-// name: "Title",
-// position: 1,
-// values: [
-// "Rosewater Kefir & Lavender",
-// "Grapefruit & Himalayan Pink",
-// "Vanilla Mint Coffee",
-// "Cedarwood & Sea Clay",
-// ],
-// }
-// ],
-// },
-// {
-// id: 4452927504426,
-// title: "Choose Bar 4",
-// handle: "choose-bar-33",
-// body_html: "<strong>***This is a hidden product used with the Product Options application, Please do not delete this product.***</strong>",
-// published_at: "2020-05-04T01:00:23-04:00",
-// created_at: "2020-05-04T01:00:23-04:00",
-// updated_at: "2020-11-29T01:51:04-05:00",
-// vendor: "Herb'N Eden",
-// product_type: "OPTIONS_HIDDEN_PRODUCT",
-// tags: [
-// "BOLD_HIDDEN_PRODUCT",
-// "OPTIONS_HIDDEN_PRODUCT",
-// ],
-// variants: [
-// {
-// id: 31543209263146,
-// title: "Grapefruit & Himalayan Pink Salt",
-// option1: "Grapefruit & Himalayan Pink Salt",
-// option2: null,
-// option3: null,
-// sku: "",
-// requires_shipping: true,
-// taxable: true,
-// featured_image: null,
-// available: true,
-// price: "1.00",
-// grams: 0,
-// compare_at_price: null,
-// position: 1,
-// product_id: 4452927504426,
-// created_at: "2020-05-04T01:00:24-04:00",
-// updated_at: "2020-11-28T17:58:06-05:00",
-// },
-// {
-// id: 31543209328682,
-// title: "Vanilla Mint Coffee",
-// option1: "Vanilla Mint Coffee",
-// option2: null,
-// option3: null,
-// sku: "",
-// requires_shipping: true,
-// taxable: true,
-// featured_image: null,
-// available: true,
-// price: "1.00",
-// grams: 0,
-// compare_at_price: null,
-// position: 2,
-// product_id: 4452927504426,
-// created_at: "2020-05-04T01:00:25-04:00",
-// updated_at: "2020-11-28T21:54:50-05:00",
-// },
-// {
-// id: 31543209394218,
-// title: "Rosewater Kefir & Lavender",
-// option1: "Rosewater Kefir & Lavender",
-// option2: null,
-// option3: null,
-// sku: "",
-// requires_shipping: true,
-// taxable: true,
-// featured_image: null,
-// available: true,
-// price: "1.00",
-// grams: 0,
-// compare_at_price: null,
-// position: 3,
-// product_id: 4452927504426,
-// created_at: "2020-05-04T01:00:26-04:00",
-// updated_at: "2020-11-28T15:41:56-05:00",
-// },
-// {
-// id: 31543209426986,
-// title: "Cedarwood & Sea Clay",
-// option1: "Cedarwood & Sea Clay",
-// option2: null,
-// option3: null,
-// sku: "",
-// requires_shipping: true,
-// taxable: true,
-// featured_image: null,
-// available: true,
-// price: "1.00",
-// grams: 0,
-// compare_at_price: null,
-// position: 4,
-// product_id: 4452927504426,
-// created_at: "2020-05-04T01:00:27-04:00",
-// updated_at: "2020-11-29T01:46:40-05:00",
-// },
-// ],
-// images: [ ],
-// options: [
-// {
-// name: "Title",
-// position: 1,
-// values: [
-// "Grapefruit & Himalayan Pink Salt",
-// "Vanilla Mint Coffee",
-// "Rosewater Kefir & Lavender",
-// "Cedarwood & Sea Clay",
-// ],
-// }
-// ],
-// }],
-// category: "Test products"
-//
-// })
+Brand.find({}, "brandName", function(err, brands){
+  if (!err){
+  let listofids = []
+  brands.forEach(function(brand){
+  listofids.push(brand._id)
+  })
+  var unique = null
+  var uniquePTypes = null
+  listofids.forEach(function(id){
+    Brand.find({_id: id}, function(err, brandproducts){
+      var alltags = []
+      var producttypes = []
+      brandproducts.forEach(function(brandproduct){
+        brandproduct.products.forEach(function(product){
+          // console.log(product.tags)
+          function onlyUnique(value, index, self) {
+            return self.indexOf(value) === index;
+          }
+          product.tags.forEach(function(tag){
+            alltags.push(tag)
+          })
+          unique = alltags.filter(onlyUnique);
+        })
+        // Brand.updateOne({_id: id}, {brandTags: unique}, function(err, results){
+        //   if (err){
+        //     console.log(err);
+        //   } else {
+        //     console.log("Succesfully updated the brandTags field")
+        //   }
+        // })
+        //
+      })
+    })
+  })
+
+
+}
+})
+Brand.find({}, "brandName", function(err, brands){
+  if (!err){
+  let listofids = []
+  brands.forEach(function(brand){
+  listofids.push(brand._id)
+  })
+  var uniquePTypes = null
+  listofids.forEach(function(id){
+    Brand.find({_id: id}, function(err, brandproducts){
+      var producttypes = []
+      brandproducts.forEach(function(brandproduct){
+        brandproduct.products.forEach(function(product){
+          // console.log(product.product_type)
+          function onlyUnique(value, index, self) {
+            return self.indexOf(value) === index;
+          }
+          producttypes.push(product.product_type)
+          uniquePTypes = producttypes.filter(onlyUnique);
+        })
+        function fix (list){
+          var finalPTypes = []
+          for (i = 0; i<list.length; i++){
+            if (list[i] != null && list[i] != "" && list[i] != undefined){
+              finalPTypes.push(list[i])
+            }
+          }
+          return finalPTypes
+        }
+        // Brand.updateOne({_id: id}, {brandProductTypes: fix(uniquePTypes)}, function(err, results){
+        //   if (err){
+        //     console.log(err);
+        //   } else {
+        //     console.log("Succesfully updated the brandProductTypes field")
+        //   }
+        // })
+        //
+      })
+    })
+  })
+
+
+}
+})
